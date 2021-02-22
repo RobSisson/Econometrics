550aff2ce323da083d66b8063695daeef6320942
rob_s <robert.b.sisson@gmail.com>
2021-02-22 18:17:44 +0000

    Updates

--- Consumer_Price_Index_Manual_Implementation.py
+++ Consumer_Price_Index_Manual_Implementation.py
@@ -4,31 +4,140 @@ from decimal import *
 
 import asyncio
 
+import calcs as c
 
-async def LPCJ_Indices(
-        dataframe
+
+def LPCDJ_Indices(
+        price_data,
+        quantity_data,
+        period_list,
 ):
-    result_df=pd.DataFrame([],
-                           columns=['Period',
-                                    'Laspeyres',
-                                    'Paasche',
-                                    'Carli',
-                                    'Jevons'])
 
-    return result_df
+    Laspeyres = [1]
+    Paasche=[1]
+    Carli = [1]
+    Dutot = [1]
+    Jevons = [1]
 
+    for i, period in enumerate(period_list):
+        if i+1 != len(period_list):
 
-async def Chain_LPCJ_Indices(
-        dataframe
+            # Laspeyres
+            price_list=price_data['Period 1'].tolist()
+            quantity_list=quantity_data['Period 1'].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Laspeyres.append(c.Laspeyres_Price_Index(price_list, quantity_list, price_2_list))
+
+            # Paasche
+            price_list=price_data['Period 1'].tolist()
+            quantity_2_list=quantity_data[period_list[i+1]].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Paasche.append(c.Paasche_Price_Index(price_list, price_2_list, quantity_2_list))
+
+            # Carli
+            price_list=price_data['Period 1'].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Carli.append(c.Carli_Price_Index(price_list, price_2_list))
+
+            # Dutot
+            price_list=price_data['Period 1'].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Dutot.append(c.Dutot_Price_Index(price_list, price_2_list))
+
+            # Jevons
+            price_list=price_data['Period 1'].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Jevons.append(c.Jevons_Price_Index(price_list, price_2_list))
+
+    result = pd.DataFrame({'Laspeyres':Laspeyres,
+                           'Paasche': Paasche,
+                           'Carli': Carli,
+                           'Dutot': Dutot,
+                           'Jevons': Jevons})
+
+    print(result)
+
+    return result
+
+price=pd.read_csv('Price.csv')
+quantity=pd.read_csv('Quantity.csv')
+
+period_list=price.columns.values
+period_list=period_list[1:]
+
+# LPCDJ_Indices(price, quantity, period_list).to_csv('pie.csv')
+
+def Chain_Period_to_Period_LPCDJ_Indices(
+        price_data,
+        quantity_data,
+        period_list,
 ):
-    result_df=pd.DataFrame([],
-                           columns=['Period',
-                                    'Chain_Laspeyres',
-                                    'Chain_Paasche',
-                                    'Chain_Carli',
-                                    'Chain_Jevons'])
+    Laspeyres=[1]
+    Paasche=[1]
+    Carli=[1]
+    Dutot=[1]
+    Jevons=[1]
 
-    return result_df
+    for i, period in enumerate(period_list):
+
+        if i+1 != len(period_list):
+            # Laspeyres
+            price_list=price_data[period_list[i]].tolist()
+            quantity_list=quantity_data[period_list[i]].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Laspeyres.append(c.Laspeyres_Price_Index(price_list, quantity_list, price_2_list))
+
+            # Paasche
+            price_list=price_data[period_list[i]].tolist()
+            quantity_2_list=quantity_data[period_list[i+1]].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Paasche.append(c.Paasche_Price_Index(price_list, price_2_list, quantity_2_list))
+
+            # Carli
+            price_list=price_data[period_list[i]].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Carli.append(c.Carli_Price_Index(price_list, price_2_list))
+
+            # Dutot
+            price_list=price_data[period_list[i]].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Dutot.append(c.Dutot_Price_Index(price_list, price_2_list))
+
+            # Jevons
+            price_list=price_data[period_list[i]].tolist()
+            price_2_list=price_data[period_list[i+1]].tolist()
+            Jevons.append(c.Jevons_Price_Index(price_list, price_2_list))
+
+        Laspeyres_final = []
+        Paasche_final = []
+        Carli_final = []
+        Dutot_final = []
+        Jevons_final = []
+
+        for i, item in enumerate(Carli):
+            Laspeyres_final.append(np.prod(Laspeyres[0:i+1]))
+
+            Paasche_final.append(np.prod(Paasche[0:i+1]))
+            Carli_final.append(np.prod(Carli[0:i+1]))
+            Dutot_final.append(np.prod(Dutot[0:i+1]))
+            Jevons_final.append(np.prod(Jevons[0:i+1]))
+
+    result=pd.DataFrame({'Period_to_Period_Laspeyres': Laspeyres,
+                         'Chain_Laspeyres': Laspeyres_final,
+                         'Period_to_Period_Paasche': Paasche,
+                         'Chain_Paasche': Paasche_final,
+                         'Period_to_Period_Carli': Carli,
+                         'Chain_Carli': Carli_final,
+                         'Period_to_Period_Dutot': Dutot,
+                         'Chain_Dutot': Dutot_final,
+                         'Period_to_Period_Jevons': Jevons,
+                         'Chain_Jevons': Jevons_final})
+
+    print(result)
+
+    return result
+# Chain_Period_to_Period_LPCDJ_Indices(price, quantity, period_list).to_csv('pizza.csv')
+
 
 
 async def Asymet_Weighted_Indices(
@@ -40,6 +149,8 @@ async def Asymet_Weighted_Indices(
                                     'Geometric_Paasche',
                                     'Geometric_Laspeyres',
                                     'Harmonic_laspeyres'])
+
+
 
     return result_df
 
